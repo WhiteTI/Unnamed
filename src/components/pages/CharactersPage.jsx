4eb7c44a275dd7e0a1d9126d6894bc88ca234cdc
white <white@exemple.com>
2024-09-11 20:47:00 +0300

    add search filter, change filters layout on grid template

--- src/components/pages/CharactersPage.jsx
+++ src/components/pages/CharactersPage.jsx
@@ -7,23 +7,25 @@ import FiltersContext from "../../context/FiltersContext.js";
 
 import classes from "../../styles/CharactersPage.module.css";
 import {Outlet, useLocation} from "react-router-dom";
+import Search from "../filters/Search.jsx";
 
 const CharactersPage = () => {
     const [elements, setElements] = useState([])
+    const [search, setSearch] = useState('')
 
     const location = useLocation()
 
     let title = location.pathname.replace('/', '')
-    title = title.at(0).toUpperCase() + title.slice(1)
 
     return (
         <>
-            <FiltersContext.Provider value={{elements, setElements}}>
+            <FiltersContext.Provider value={{elements, setElements, search, setSearch}}>
                 <div className={`bg-fixed ${classes.mainBgImage}`}>
                     <div className='container mx-auto pt-56'>
                         <h1 className={`${classes.title} font-bold uppercase`}>{title} List</h1>
-                        <div className='flex justify-center'>
-                            <ElementFilter/>
+                        <div className='grid grid-cols-[130px_180px_470px_360px] justify-between'>
+                            <ElementFilter grid={'col-start-3 col-end-4'}/>
+                            <Search grid={'col-start-4'}/>
                         </div>
                     </div>
                 </div>
