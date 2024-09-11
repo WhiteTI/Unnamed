4f6f5eb0eb15969774820fa16eb67c94b9c46c79
white <white@exemple.com>
2024-09-11 20:40:32 +0300

    create search filter component on main page and css file for this component

--- src/components/filters/Search.jsx
+++ src/components/filters/Search.jsx
@@ -1,8 +1,29 @@
+import {useContext} from "react";
+import {useLocation} from "react-router-dom";
+import FiltersContext from "../../context/FiltersContext.js";
 
-const Search = () => {
-    return (
-        <div>
+import classes from '../../styles/filters/Search.module.css'
+import searchIcon from "../../assets/img/search.svg"
 
+const Search = ({grid}) => {
+    const {search, setSearch} = useContext(FiltersContext)
+    const location = useLocation()
+    const show = location.pathname === '/characters'
+
+    return show && (
+        <div className={`${grid} flex items-center p-3 ${classes.searchWrapper}`}>
+            <label htmlFor='search'>
+                <img src={searchIcon} alt=""/>
+            </label>
+
+            <input
+                className={`bg-transparent ml-3 ${classes.search}`}
+                id='search'
+                type="search"
+                placeholder='Search'
+                value={search}
+                onChange={event => setSearch(event.target.value)}
+            />
         </div>
     );
 };
