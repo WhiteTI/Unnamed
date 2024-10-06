a2bd6f7d0ef4d26cfdbe7a1f7a401f407d5edb60
white <white@exemple.com>
2024-10-06 19:55:05 +0300

    delete placeholder elem

--- src/components/artifactsList/ArtifactsList.jsx
+++ src/components/artifactsList/ArtifactsList.jsx
@@ -2,6 +2,7 @@ import {useQuery} from "@tanstack/react-query";
 import {getAllArtifacts} from "../../lib/appwrite.js";
 import Loading from "../loading/Loading.jsx";
 import classes from "../../styles/ArtifactsList.module.css";
+import parse from "html-react-parser";
 
 const ArtifactsList = () => {
     const {isPending, data} = useQuery({
@@ -30,112 +31,7 @@ const ArtifactsList = () => {
                         </div>
 
                         <div className={`overflow-auto p-3 ${classes.cardDesc}`}>
-                            <p>{artifact.effect}</p>
-                        </div>
-                    </div>
-                ))
-            }
-            {
-                data.map(artifact => (
-                    <div key={artifact.$id} className={`rounded-t-lg ${classes.card}`}>
-
-                        <div className={`grid grid-cols-2 items-center gap-x-2 rounded-t-lg pr-3 ${classes.cardTitle}`}
-                             style={{backgroundColor: artifact.rarity == 5 ? 'var(--5-star-color)' : 'var(--4-star-color)'}}
-                        >
-                            <img src={artifact.image} alt=""/>
-
-                            <div className='text-lg font-medium'>
-                                <p className='text-xl'>{artifact.name}</p>
-                                <p className='font-semibold text-xl'>{artifact.rarity}&#9733;</p>
-                            </div>
-                        </div>
-
-                        <div className={`overflow-auto p-3 ${classes.cardDesc}`}>
-                            <p>{artifact.effect}</p>
-                        </div>
-                    </div>
-                ))
-            }
-            {
-                data.map(artifact => (
-                    <div key={artifact.$id} className={`rounded-t-lg ${classes.card}`}>
-
-                        <div className={`grid grid-cols-2 items-center gap-x-2 rounded-t-lg pr-3 ${classes.cardTitle}`}
-                             style={{backgroundColor: artifact.rarity == 5 ? 'var(--5-star-color)' : 'var(--4-star-color)'}}
-                        >
-                            <img src={artifact.image} alt=""/>
-
-                            <div className='text-lg font-medium'>
-                                <p className='text-xl'>{artifact.name}</p>
-                                <p className='font-semibold text-xl'>{artifact.rarity}&#9733;</p>
-                            </div>
-                        </div>
-
-                        <div className={`overflow-auto p-3 ${classes.cardDesc}`}>
-                            <p>{artifact.effect}</p>
-                        </div>
-                    </div>
-                ))
-            }
-            {
-                data.map(artifact => (
-                    <div key={artifact.$id} className={`rounded-t-lg ${classes.card}`}>
-
-                        <div className={`grid grid-cols-2 items-center gap-x-2 rounded-t-lg pr-3 ${classes.cardTitle}`}
-                             style={{backgroundColor: artifact.rarity == 5 ? 'var(--5-star-color)' : 'var(--4-star-color)'}}
-                        >
-                            <img src={artifact.image} alt=""/>
-
-                            <div className='text-lg font-medium'>
-                                <p className='text-xl'>{artifact.name}</p>
-                                <p className='font-semibold text-xl'>{artifact.rarity}&#9733;</p>
-                            </div>
-                        </div>
-
-                        <div className={`overflow-auto p-3 ${classes.cardDesc}`}>
-                            <p>{artifact.effect}</p>
-                        </div>
-                    </div>
-                ))
-            }
-            {
-                data.map(artifact => (
-                    <div key={artifact.$id} className={`rounded-t-lg ${classes.card}`}>
-
-                        <div className={`grid grid-cols-2 items-center gap-x-2 rounded-t-lg pr-3 ${classes.cardTitle}`}
-                             style={{backgroundColor: artifact.rarity == 5 ? 'var(--5-star-color)' : 'var(--4-star-color)'}}
-                        >
-                            <img src={artifact.image} alt=""/>
-
-                            <div className='text-lg font-medium'>
-                                <p className='text-xl'>{artifact.name}</p>
-                                <p className='font-semibold text-xl'>{artifact.rarity}&#9733;</p>
-                            </div>
-                        </div>
-
-                        <div className={`overflow-auto p-3 ${classes.cardDesc}`}>
-                            <p>{artifact.effect}</p>
-                        </div>
-                    </div>
-                ))
-            }
-            {
-                data.map(artifact => (
-                    <div key={artifact.$id} className={`rounded-t-lg ${classes.card}`}>
-
-                        <div className={`grid grid-cols-2 items-center gap-x-2 rounded-t-lg pr-3 ${classes.cardTitle}`}
-                             style={{backgroundColor: artifact.rarity == 5 ? 'var(--5-star-color)' : 'var(--4-star-color)'}}
-                        >
-                            <img src={artifact.image} alt=""/>
-
-                            <div className='text-lg font-medium'>
-                                <p className='text-xl'>{artifact.name}</p>
-                                <p className='font-semibold text-xl'>{artifact.rarity}&#9733;</p>
-                            </div>
-                        </div>
-
-                        <div className={`overflow-auto p-3 ${classes.cardDesc}`}>
-                            <p>{artifact.effect}</p>
+                            <p>{parse(artifact.effect.replace('\n', '<br/>'))}</p>
                         </div>
                     </div>
                 ))
