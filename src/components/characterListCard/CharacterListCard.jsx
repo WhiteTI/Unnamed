c30ec94c14db4b74babdbd01da7f946ea6e7d9e9
white <white@exemple.com>
2024-10-06 20:03:43 +0300

    remove useless func and add hover animation

--- src/components/characterListCard/CharacterListCard.jsx
+++ src/components/characterListCard/CharacterListCard.jsx
@@ -7,8 +7,6 @@ const CharacterListCard = ({name, element, img, weapon, rarity, id}) => {
 
     const rarityColor = rarity == 5 ? 'var(--5-star-color)' : 'var(--4-star-color)'
 
-    // const {elemImg} = useElement(element)
-
     return (
         <Link to={`${id}/overview`}>
             <div className={`flex flex-col justify-end px-2.5 py-2 ${classes.card}`} style={{backgroundImage: `url(${img})`, backgroundColor: rarityColor}}>
