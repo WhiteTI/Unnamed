baac9554c2319c91940452a74586040c88fbb35b
white <white@exemple.com>
2024-10-13 22:19:44 +0300

    add background div to element image and remove useElement

--- src/components/characterListCard/CharacterListCard.jsx
+++ src/components/characterListCard/CharacterListCard.jsx
@@ -1,8 +1,6 @@
 import {Link} from "react-router-dom";
 import classes from '../../styles/CharacterListCard.module.css'
 
-import useElement from "../../hooks/useElement.jsx";
-
 const CharacterListCard = ({name, element, img, weapon, rarity, id}) => {
 
     const rarityColor = rarity == 5 ? 'var(--5-star-color)' : 'var(--4-star-color)'
@@ -10,7 +8,9 @@ const CharacterListCard = ({name, element, img, weapon, rarity, id}) => {
     return (
         <Link to={`${id}/overview`}>
             <div className={`flex flex-col justify-end px-2.5 py-2 ${classes.card}`} style={{backgroundImage: `url(${img})`, backgroundColor: rarityColor}}>
-                <img src={element.image} alt="svg"/>
+                <div>
+                    <img src={element.image} alt="svg"/>
+                </div>
                 <p className='text-xl font-semibold z-10'>{name}</p>
                 <u className='text-xl z-10'>{weapon.name}</u>
             </div>
