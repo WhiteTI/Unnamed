2f69128c13aa8b7a42e9d4bf42034346596745f2
white <white@exemple.com>
2024-10-13 22:17:45 +0300

    add filtering, suspense to show loading when data is Loading, and fix background to all rarity colors

--- src/components/artifactsList/ArtifactsList.jsx
+++ src/components/artifactsList/ArtifactsList.jsx
@@ -1,26 +1,53 @@
-import {useQuery} from "@tanstack/react-query";
+import {useSuspenseQuery} from "@tanstack/react-query";
 import {getAllArtifacts} from "../../lib/appwrite.js";
-import Loading from "../loading/Loading.jsx";
-import classes from "../../styles/ArtifactsList.module.css";
 import parse from "html-react-parser";
+import {useContext, useEffect, useState} from "react";
+
+import Loading from "../loading/Loading.jsx";
+import FiltersContext from "../../context/FiltersContext.js";
+import classes from "../../styles/ArtifactsList.module.css";
 
 const ArtifactsList = () => {
-    const {isPending, data} = useQuery({
+    const [artifacts, setArtifacts] = useState([])
+
+    const {search, rarityFilter} = useContext(FiltersContext)
+
+    const {data} = useSuspenseQuery({
         queryKey: ['artifacts'],
         queryFn: () => getAllArtifacts()
             .then(response => response.documents)
     })
 
-    if (isPending) return <Loading/>
+    useEffect(() => {
+        setArtifacts(filtering)
+    }, [search.artifacts, rarityFilter.artifacts]);
+
+    function filtering() {
+        if (!search.artifacts && !rarityFilter.artifacts.length)
+            return data
+
+        let newData = data
+
+        if (rarityFilter.artifacts.length)
+            newData = newData.filter(({rarity}) => rarityFilter.artifacts.includes(+rarity))
+
+        return newData.filter(({name}) => name.toLowerCase().includes(search.artifacts.toLowerCase()))
+    }
 
     return (
         <div className='grid xl:grid-cols-4 md:grid-cols-3 auto-rows-auto justify-evenly items-stretch gap-4 '>
             {
-                data.map(artifact => (
+                artifacts.map(artifact => (
                     <div key={artifact.$id} className={`rounded-t-lg ${classes.card}`}>
 
                         <div className={`grid grid-cols-[160px_minmax(0,_1fr)] items-center gap-x-2 rounded-t-lg pr-3 ${classes.cardTitle}`}
-                             style={{backgroundColor: artifact.rarity == 5 ? 'var(--5-star-color)' : 'var(--4-star-color)'}}
+                             style={{
+                                 backgroundColor: artifact.rarity == 5
+                                     ? 'var(--5-star-color)'
+                                     : artifact.rarity == 4
+                                     ? 'var(--4-star-color)'
+                                         : 'var(--3-star-color)'
+                            }}
                         >
                             <img src={artifact.image} alt=""/>
 
